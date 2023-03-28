e89e850c60f954241515697026516f5274edb5d4
Majd1945 <majdadeeb96@gmail.com>
2023-03-29 00:10:59 +0300

    cart page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ShoppingCart from "./pages/ShoppingCart";
 import { Rout, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <Routes>
       <Route exact path="/login" element={<Login/>} />
       <Route exact path="/register" element={<Register/>} />
+      <Route exact path="/cart" element={<ShoppingCart/>} />
     </Routes>
   );
 }

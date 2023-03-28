5650e4069859ade63c4da5f6174a4e91e0b9c09f
Mohammad Almosallam <moh.almusallam4@gmail.com>
2023-03-29 00:20:39 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { Rout, Route, Routes } from "react-router-dom";
+import Test from "./pages/Test";
 function App() {
   return (
     <Routes>
-      <Route exact path="/login" element={<Login/>} />
-      <Route exact path="/register" element={<Register/>} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/register" element={<Register />} />
+      <Route exact path="/test" element={<Test />} />
     </Routes>
   );
 }
